f8700e392e0fabfaf4b1397afbdbd3327c42ff4b
sniffjoke <sniffjoke@gmail.com>
2024-08-11 16:34:53 +0200

    slice

---
+++ index.js
@@ -0,0 +1,26 @@
+// let str = 'Hello, world!';
+// let startIndex = 7
+
+// const getSubStr = (str, i) => {
+//     return str.slice(i)
+// }
+
+// console.log(getSubStr(str, startIndex))
+
+// function getLastElements(arr, n) {
+//     return arr.slice(-n)
+// }
+//
+// const arr = [1, 2, 3, 4, 5]
+//
+// console.log(getLastElements(arr, 2))
+
+// function extractPath(url, index) {
+//     return url.slice(index+1)
+// }
+
+// const strUrl = "https://example.com/blog/article"
+
+// console.log(extractPath(strUrl, 18))
+
+
